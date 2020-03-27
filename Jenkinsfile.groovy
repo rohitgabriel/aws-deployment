f7f1529ace2a02caa3047e02efa8ed49305f9d64
unknown <suzieboo@gmail.com>
2020-03-27 22:41:42 +1300

    ssh steps6

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -29,7 +29,7 @@ pipeline {
                 sh './get-instance-id.sh'
                 }
                 sshagent(credentials : ['awskey']) {
-                sh 'ssh -o StrictHostKeyChecking=no ubuntu@$instance_ip uptime'
+                sh 'ssh -o StrictHostKeyChecking=no ubuntu@${instance_ip} uptime'
                 sh 'scp ./deploycode.sh ubuntu@13.54.226.2:/tmp/deploycode.sh'
                 sh 'ssh ubuntu@13.54.226.2 chmod 755 /tmp/deploycode.sh'
                 sh 'ssh ubuntu@13.54.226.2 /tmp/deploycode.sh'
